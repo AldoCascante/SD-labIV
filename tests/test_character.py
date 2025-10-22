fe46c030eabd54126453272e5c6a8f0d8579699d
Aldo Cascante <cascante.aldo@gmail.com>
2025-10-21 19:30:58 -0600

    refactored test_take_damage_reduces_health_and_can_kill into 2 different tests

--- tests/test_character.py
+++ tests/test_character.py
@@ -9,11 +9,16 @@ class TestCharacter(unittest.TestCase):
         self.assertEqual(warrior.health, 100)
         self.assertTrue(warrior.is_alive)
 
-    def test_take_damage_reduces_health_and_can_kill(self):
+    def test_take_damage_reduces_health(self):
         """Test que el daño reduce salud y puede matar al personaje"""
         mage = Character("Gandalf")
         mage.take_damage(30)
         self.assertEqual(mage.health, 70)
         
-        mage.take_damage(70)
+
+    def test_take_damage_can_kill(self):
+        """Test que el daño reduce salud y puede matar al personaje"""
+        mage = Character("Gandalf")
+        mage.take_damage(100)
+        self.assertEqual(mage.health, 0)
         self.assertFalse(mage.is_alive)
